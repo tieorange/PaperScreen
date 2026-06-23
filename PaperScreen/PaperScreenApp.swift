9e44cecbe8424ffea66791d9474bbc3263852cc1
Andrii Kovalchuk <tieorange@gmail.com>
2026-06-23 17:21:42 +0300

    Initial Commit

---
+++ PaperScreen/PaperScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperScreenApp.swift
+//  PaperScreen
+//
+//  Created by anduser on 23.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct PaperScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
